25469699902070cdad73529ff6a4520c1cb02819
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2025-12-05 12:47:29 -0500

    init koin

--- app/src/main/java/com/kotlin/test/MainActivity.kt
+++ app/src/main/java/com/kotlin/test/MainActivity.kt
@@ -4,10 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.kotlin.test.util.navigation.AppNavigation
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Koin
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@MainActivity)
+            modules(emptyList()) // Add your modules here when you create them
+        }
+
         setContent {
             AppNavigation()
         }
