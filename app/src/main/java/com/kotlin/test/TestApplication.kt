fc5b8b0d58b2392b61ffd5c6684c78fe3a6dbe6c
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2025-12-14 15:25:15 -0500

    start application

--- app/src/main/java/com/kotlin/test/TestApplication.kt
+++ app/src/main/java/com/kotlin/test/TestApplication.kt
@@ -1,11 +1,8 @@
 package com.kotlin.test
 
 import android.app.Application
-import com.kotlin.test.feature.counter.counterModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 
 class TestApplication : Application() {
     override fun onCreate() {
@@ -13,9 +10,8 @@ class TestApplication : Application() {
 
         // Initialize Koin once for the entire app lifecycle
         startKoin {
-            androidLogger(Level.ERROR)
             androidContext(this@TestApplication)
-            modules(listOf(counterModule))
+            modules(listOf())
         }
     }
 }
